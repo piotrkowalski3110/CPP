7ca9b70bfa52fe66af74bd17f18f01572c1dce71
pkowalski3110 <piotr.kowalski3110@gmail.com>
2022-05-18 09:43:24 +0200

    agregaty

--- Agregaty lab8/main.cpp
+++ Agregaty lab8/main.cpp
@@ -39,13 +39,19 @@ public:
         cout << "Wiek: " << wiek << " lat" << endl;
         cout << "Stanowisko: " << stanowisko << endl << endl;
     }
+
+    friend ostream &operator<<(ostream &out, const pracownik &p) {
+        out << p.imie << " " << p.nazwisko << endl << "PESEL: " << p.pesel << endl << "Wiek: " << p.wiek << " lat"
+            << endl << "Stanowisko: " << p.stanowisko << endl << endl;
+        return out;
+    }
 };
 
 
 template<typename TYP> class ListaTab
 {
-    TYP T[maxlength];
-    int Last;  //indeks ostatniego elementu listy
+    TYP T[10];
+    int Last;
 public:
     ListaTab(){Last=-1;}
     int End();
@@ -59,61 +65,51 @@ public:
     void print();
 };
 
-template<typename TYP> int ListaTab<TYP>::End()
-{
-    return Last+1;
+template<typename TYP> int ListaTab<TYP>::End() {
+    return Last + 1;
 }
 
-template<typename TYP> int ListaTab<TYP>::First()
-{
+template<typename TYP> int ListaTab<TYP>::First() {
     return 0;
 }
 
-template<typename TYP> int ListaTab<TYP>::Next(int P)
-{
-    if (P<End())
-    {
+template<typename TYP> int ListaTab<TYP>::Next(int P) {
+    if (P < End()) {
         return P + 1;
-    }
-    else cout << "Podany indeks jest poza zakresem listy." << endl;return NULL;
+    } else cout << "Podany indeks jest poza zakresem listy." << endl;
+    return NULL;
 }
 
-template<typename TYP> int ListaTab<TYP>::Previous(int P)
-{
-    if ((P <= First()) || (P > End())) {cout << "Podany indeks jest poza zakresem listy." << endl;return NULL;}
+template<typename TYP> int ListaTab<TYP>::Previous(int P) {
+    if ((P <= First()) || (P > End())) {
+        cout << "Podany indeks jest poza zakresem listy." << endl;
+        return NULL;
+    }
     else return P - 1;
 }
 
-template<typename TYP> int ListaTab<TYP>::Locate(TYP X)
-{
-    for (int i = 0; i<End(); i++)
+template<typename TYP> int ListaTab<TYP>::Locate(TYP X) {
+    for (int i = 0; i < End(); i++)
         if (T[i] == X)return i;
     return End();
 }
 
-template<typename TYP> TYP ListaTab<TYP>::Retrieve(int P)
-{
-    if (P >= First() && P <= End())
-    {
+template<typename TYP> TYP ListaTab<TYP>::Retrieve(int P) {
+    if (P >= First() && P <= End()) {
         return T[P];
-    }
-    else return T[0];
+    } else return T[0];
 }
 
-template<typename TYP> bool ListaTab<TYP>::Insert(TYP X, int P)
-{
+template<typename TYP> bool ListaTab<TYP>::Insert(TYP X, int P) {
     if ((P < First()) || (P > End()))
         return false;
-    if (P == End())
-    {
+    if (P == End()) {
         T[P] = X;
         Last = Last + 1;
         return true;
     }
-    if (P<End())
-    {
-        for (int k = End(); k>P; k--)
-        {
+    if (P < End()) {
+        for (int k = End(); k > P; k--) {
             T[k] = T[k - 1];
         }
         T[P] = X;
@@ -123,10 +119,8 @@ template<typename TYP> bool ListaTab<TYP>::Insert(TYP X, int P)
     return false;
 }
 
-template<typename TYP> bool ListaTab<TYP>::Delete(int P)
-{
-    while (P<End() - 1)
-    {
+template<typename TYP> bool ListaTab<TYP>::Delete(int P) {
+    while (P < End() - 1) {
         T[P] = T[P + 1];
         P++;
     }
@@ -134,11 +128,9 @@ template<typename TYP> bool ListaTab<TYP>::Delete(int P)
     return false;
 }
 
-template<typename TYP> void ListaTab<TYP>::print()
-{
+template<typename TYP> void ListaTab<TYP>::print() {
     int i = First();
-    while (i != End())
-    {
+    while (i != End()) {
         cout << Retrieve(i) << " ";
         i = Next(i);
     }
@@ -152,8 +144,5 @@ int main() {
     pracownik Piotr = {"Piotr", "Kowalski", 123456789, 21, "Kierowca"};
     pracownik Borys = {"Borys", "Siarek", 987654321, 22, "Logistyk"};
     pracownik Maciej = {"Maciej", "Zieliński", 1234098765, 23, "Manager"};
-    Piotr.druk();
-    Borys.druk();
-    Maciej.druk();
-
+    cout << Piotr << Borys << Maciej;
 }
